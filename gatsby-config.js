1d22913aa0d35b87e0f80e21e34bf8bf9e980ae4
Iiro Alhonen <iiro.alhonen@basemark.com>
2020-04-09 18:17:17 +0300

    Setting up.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Bean Juice website',
+    author: 'Iiro Alhonen',
+    description: 'Bean Juice home page',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
